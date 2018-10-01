898cfdd3c77084eedb93145746c21198a18338c5
fddata <fraserjdouglas@gmail.com>
2018-10-01 22:26:53 +0100

    adds StockMarket and StockMarketTest classes

--- src/main/java/StockMarket.java
+++ src/main/java/StockMarket.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+
 public class StockMarket {
+
+    private ArrayList<Trade> trades;
+
+    public StockMarket(ArrayList<Trade> trades) {
+        this.trades = trades;
+    }
+
+    public ArrayList<Trade> getTrades() {
+        return trades;
+    }
+
+    public void addTrade(Trade trade){
+        this.trades.add(trade);
+    }
+
+    public int getNumberOfTrades(){
+        return this.trades.size();
+    }
+
+
+    public  double calculateGeometricMean(){
+
+//        first get an array of prices for each trade:
+
+        double[] priceArray = new double[this.getNumberOfTrades()];
+        for (int i = 0; i <= this.getNumberOfTrades(); i++ ){
+             priceArray[i] = this.trades.get(i).getTradedPrice();
+        }
+//        then multiply these prices together:
+
+        double reducerSum = priceArray[0];
+        for(int i = 1; i < priceArray.length; i++){
+            reducerSum *= priceArray[i];
+        }
+
+//        then take the 1/n power of this sum:
+
+        return Math.pow(reducerSum, 1 / priceArray.length);
+
+    }
 }
--- src/test/java/StockMarketTest.java
+++ src/test/java/StockMarketTest.java
@@ -1,5 +1,29 @@
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class StockMarketTest {
 
+    CommonStock stockTEA;
+    CommonStock stockPOP;
+    CommonStock stockALE;
+    PreferredStock stockGIN;
+    CommonStock stockJOE;
+
+    Trade tradeTEA;
+    Trade tradePOP;
+    Trade tradeALE;
+    Trade tradeGIN;
+    Trade tradeJOE;
+
+    StockMarket stockMarket;
+
+    @Before
+    public void setUp() {
+        stockTEA = new CommonStock("TEA", 0,100);
+        stockPOP = new CommonStock("POP", 8,100);
+        stockALE = new CommonStock("ALE", 23,60);
+        stockGIN = new PreferredStock("GIN", 23,60, 0.02);
+        stockJOE = new CommonStock("JOE", 13,250);
+    }
 }
